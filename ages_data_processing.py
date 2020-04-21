8229d5086c6293494e7c9c6a2479bab2c0cb8e9a
Jainam Shah <43648181+js0753@users.noreply.github.com>
2020-04-22 01:46:26 +0530

    Update ages_data_processing.py

--- ages_data_processing.py
+++ ages_data_processing.py
@@ -1,5 +1,6 @@
 import ages_scan
 import pandas as pd
+from csv import writer 
 
 def entry_check(text):
     resDb=pd.read_csv('Resident Database.csv')
@@ -15,8 +16,16 @@ def entry_check(text):
     return flag
 
 def vis_entry_log(vno,name,flat):
-      with open('visitor_log.csv','a') as f:
-            df.to_csv(f,header=False)
-            df.append([vno,name,flat])
+      print(vno,name,flat)
+      
+      with open('visitor_log.csv','a+',newline='') as f:
+            cw=writer(f)
+            cw.writerow([vno,name,flat,"",""])
+
+      return 1
+            
+
+#vis_entry_log("AM1234","Strange","B221")
+#print("Success")
             
             
