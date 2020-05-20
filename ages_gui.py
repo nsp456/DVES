f55c10f3654ea28de0dbb3db2d6f2a530b47af66
Jainam Shah <43648181+js0753@users.noreply.github.com>
2020-05-21 03:59:42 +0530

    Updated for Report Generation

--- ages_gui.py
+++ ages_gui.py
@@ -3,13 +3,14 @@ from ages_data_processing import *
 import sys
 import os.path
 from os import path
+from datetime import datetime,date as dt
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 try:
     from PyQt5 import QtCore
-    from PyQt5.QtCore import Qt
-    from PyQt5.QtWidgets import QMainWindow,QApplication,QGridLayout,QLineEdit,QPushButton,QVBoxLayout,QWidget,QStackedWidget,QLabel,QMessageBox,QDialog
-   
+    from PyQt5.QtCore import Qt,QDate,QTime
+    from PyQt5.QtWidgets import QMainWindow,QApplication,QGridLayout,QLineEdit,QPushButton,QVBoxLayout,QWidget,QStackedWidget,QLabel,QMessageBox,QDialog,QComboBox,QDateEdit,QTimeEdit,QTableWidget,QTableWidgetItem
+    
   
     
     
@@ -18,8 +19,8 @@ except ImportError:
     install('pyqt5')
    
    
-    from PyQt5.QtCore import Qt,QLabel
-    from PyQt5.QtWidgets import QMainWindow,QApplication,QGridLayout,QLineEdit,QPushButton,QVBoxLayout,QWidget,QStackedWidget,QMessageBox
+    from PyQt5.QtCore import Qt
+    from PyQt5.QtWidgets import QMainWindow,QApplication,QGridLayout,QLineEdit,QPushButton,QVBoxLayout,QWidget,QStackedWidget,QLabel,QMessageBox,QDialog,QComboBox
    
 
 from functools import partial
@@ -30,7 +31,7 @@ class PageWindow(QMainWindow):
     def goto(self, name):
         self.gotoSignal.emit(name)
 
-
+#-----------------------------------------------------------------------
 class MainWindow(PageWindow):
     password=""
     username=""
@@ -76,9 +77,10 @@ class MainWindow(PageWindow):
             if button == "logIn":
                 if(str(self.pwd.text())==str(password) and self.unm.text()==username):
                     print("Logged In Successfully!")
+                    #clear password text
                     self.goto("welcome")
                 else:
-                    QMessageBox.about(self,"Log In Failed","Incorrect Credentials\nPlease Re-enter")
+                    QMessageBox.about(self,"Log In Failed","Wrong Credentials\nPlease Re-enter")
                     self.pwd.setText("")
             elif button== "Reg":
                 
@@ -92,7 +94,7 @@ class MainWindow(PageWindow):
             
         return handleButton
 
-
+#--------------------------------------------------------------------------------------------
 class RegisterUser(PageWindow):
     def __init__(self):
         
@@ -139,11 +141,15 @@ class RegisterUser(PageWindow):
                 QMessageBox.about(self,"Success","Registered Successfully")
                 self.goto("welcome")
                     
-                                    
+               
+                    
+            
         return handleButton
 
 
-    
+      
+#--------------------------------------------------------------------------------------------------------
+
 class WelcomeUser(PageWindow):
     def __init__(self):
         super().__init__()
@@ -202,34 +208,34 @@ class WelcomeUser(PageWindow):
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
 
-class SetupConnection:
-    
-
-    def __init__(self, view):
-       
-        self._view = view
-        # Connect signals and slots
-        for btnText, btn in self._view.buttons.items():
+        for btnText, btn in self.buttons.items():
             if(btnText=="Entry Scan"):
                 btn.clicked.connect(partial(self.button_press_entry, btnText))
             if(btnText=="Exit Scan"):
                 btn.clicked.connect(partial(self.button_press_exit, btnText))
+            
+            if(btnText=="Report Generation"):
+                btn.clicked.connect(self.gotoReport)
+            
 
      
    
      
+    def gotoReport(self):
+        self.goto("report")
+        
     def button_press_entry(self, sub_exp):
       print("\nClicked ",sub_exp)
       text=ages_scan.scan()
       if(text!=""):
           message="Plate Number is : "+text
-          QMessageBox.about(self._view, "Scan Successfull",message)
+          QMessageBox.about(self, "Scan Successfull",message)
       flag=entry_check(text)
       if(flag==-1):
-          QMessageBox.about(self._view, "Failure","Vehicle already Inside. Please try Exit Scan")
+          QMessageBox.about(self, "Failure","Vehicle already Inside,Please Try Exit Scan")
 
       if(flag==0):
-          vform=QDialog(parent=self._view)
+          vform=QDialog(parent=self)
           vform.setWindowTitle("Visitor Detials")
           vform.setModal(True)
           vform.v_name=QLineEdit(vform)
@@ -252,14 +258,107 @@ class SetupConnection:
       text=ages_scan.scan()
       if(text!=""):
           message="Plate Number is : "+text
-          QMessageBox.about(self._view, "Scan Successfull",message)
+          QMessageBox.about(self, "Scan Successfull",message)
           flag=exit_log(text)
+          print(flag)
           if(flag==-1):
-              QMessageBox.about(self._view, "Failure","Vehicle not present in premises. Please Try Entry Scan")
+              QMessageBox.about(self, "Failure","Vehicle already Outside,Please Try Entry Scan")
       else:
-          QMessageBox.about(self._view, "Scan Failed","Please Try again")
+          QMessageBox.about(self, "Scan Failed","Please Try again")
+
+#-----------------------------------------------------------------------------
+
+class ReportGen(PageWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.createWidgets()
+
+    def initUI(self):
+        self.setWindowTitle("Report Generation")
+        self.setFixedSize(400, 590)
+        # Set the central widget and the general layout
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+
+    def createWidgets(self):
+        self.logSelector=QComboBox(self)
+        self.logSelector.resize(110,40)
+        self.logSelector.move(140,40)
+        self.logSelector.addItem("Resident Log")
+        self.logSelector.addItem("Visitor Log")
+        self.datepicker=QDateEdit(self)
+        self.datepicker.setCalendarPopup(True)
+        self.datepicker.move(145,120)
+        
+        self.datepicker.setDate(dt.today())
+        self.startTime=QTimeEdit(self)
+        self.startTime.move(65,200)
+        self.endTime=QTimeEdit(self)
+        self.endTime.move(225,200)
+        self.genButton=QPushButton("Generate",self)
+        self.genButton.move(145,280)
+        self.backButton=QPushButton("Back",self)
+        self.backButton.move(145,340)
+        self.backButton.clicked.connect(self.goBack)
+        self.genButton.clicked.connect(self.repGen)
+    def goBack(self):
+        self.goto("welcome")
+
+    def repGen(self):
+        #print("Hey")
+        log=self.logSelector.currentText()
+        date=str(self.datepicker.date().toPyDate())
+        #print(date)
+        start=str(self.startTime.time().toPyTime())
+        end=str(self.endTime.time().toPyTime())
+        vbox=QVBoxLayout()
+        table=QTableWidget()
+        #print("Heyo")
+        #print(log+" "+date+" "+start+" "+end)
+        repList=getRepData(log,date,start,end)
+        rows=len(repList)
+        print(repList)
+        table.setRowCount(rows+1)
+        if(log=="Resident Log"):
+            #print("hey")
+            table.setColumnCount(4)
+            table.setItem(0,0,QTableWidgetItem("Vehicle No."))
+            table.setItem(0,1,QTableWidgetItem("Flat No."))
+            table.setItem(0,2,QTableWidgetItem("Entry Time"))
+            table.setItem(0,3,QTableWidgetItem("Exit Time"))
+            table.resize(650,400)
+            
+        else:
+            table.setColumnCount(5)
+            table.setItem(0,0,QTableWidgetItem("Vehicle No."))
+            table.setItem(0,1,QTableWidgetItem("Flat No."))
+            table.setItem(0,2,QTableWidgetItem("Visitor Name"))
+            table.setItem(0,3,QTableWidgetItem("Entry Time"))
+            table.setItem(0,4,QTableWidgetItem("Exit Time"))
+            table.resize(800,400)
+
+        try:
+            i=1
+            j=0
+            for row in repList:
+                for j in range(len(row)):
+                    #print(j+" "+row[j])
+                    table.setItem(i,j,QTableWidgetItem(row[j]))
+                i+=1
+        except Exception as e:
+            print(e)
+        
+        table.horizontalHeader().setDefaultSectionSize(155)
+        table.show()
+        vbox.addWidget(table)
+        self.setLayout(vbox)
         
         
+        
+#------------------------------------------------------------------------------
 
 class Window(QMainWindow):
     def __init__(self, parent=None):
@@ -273,6 +372,7 @@ class Window(QMainWindow):
         self.register(MainWindow(), "main")
         self.register(WelcomeUser(), "welcome")
         self.register(RegisterUser(),"register")
+        self.register(ReportGen(),"report")
         self.goto("main")
 
     def register(self, widget, name):
@@ -287,7 +387,7 @@ class Window(QMainWindow):
             widget = self.m_pages[name]
             self.stacked_widget.setCurrentWidget(widget)
             self.setWindowTitle(widget.windowTitle())
-
+#-----------------------------------------------------------------------------------------------
 
 def main():
     
@@ -296,8 +396,8 @@ def main():
     app = QApplication(sys.argv)
  
     view = Window()
-    wel=view.m_pages["welcome"]
-    SetupConnection(view=wel)
+    #wel=view.m_pages["welcome"]
+    #SetupConnection(view=wel)
     view.show()
     
     #SetupConnection( view=view)
